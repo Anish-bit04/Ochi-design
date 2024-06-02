70daa00f858a6d27950b7230c027a2ef123de892
Anish <Anish@lamarr.tech>
2024-06-02 21:19:53 +0530

    added eyes animation

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Marquee from './components/Marquee'
 import About from './components/About'
+import Eyes from './components/Eyes'
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
     <Hero/>
     <Marquee/>
     <About/>
+    <Eyes />
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'eyes': "url('/public/eyes.jpg')",
+      }
+    },
   },
   plugins: [],
 }
