cd8be6c8d76b0e5ab48cadb49aee1967cbd15e2e
Anish <Anish@lamarr.tech>
2024-06-02 19:04:33 +0530

    create marquee section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Marquee from './components/Marquee'
 
 const App = () => {
   return (
     <div className='w-full h-screen  text-black '>
     <Navbar/>  
     <Hero/>
+    <Marquee/>
     </div>
   )
 }
---
+++ src/components/Marquee.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Marquee = () => {
+  return (
+    <div className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+        <div className="text border-t-[1px] border-b-[1px] border-zinc-300 flex gap-10 overflow-hidden whitespace-nowrap ">
+            <h1 className='text-[24vw] leading-none font-[MADE Outer Sans Alt] uppercase -mt-16 -mb-6 font-bold text-white'>we are ochi</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Marquee
