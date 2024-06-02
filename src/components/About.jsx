e95a69c03c1b1bc5ea1ec9959fa50193af35a105
Anish <Anish@lamarr.tech>
2024-06-03 05:13:40 +0530

    coorected text sixe and colur

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='w-full p-20 bg-[#cdea68] rounded-tl-3xl rounded-tr-3xl text-black'>
-        <h1 className='font-["Neue_Montreal"] text-[4vw] leading-[4.5vw] tracking-tighter '>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
+    <div className='w-full p-20 bg-[#cdea68] rounded-tl-3xl rounded-tr-3xl text-zinc-800'>
+        <h1 className='font-["Neue_Montreal"] text-[3.75vw] leading-[4.5vw] tracking-tight '>Ochi is a strategic partner for fast-growing tech businesses that need to raise funds, sell products, explain complex ideas, and hire great people.</h1>
         <div className='w-full flex gap-5 border-t-[1px] pt-10 mt-20 border-[#a1b562]'>
           <div className='w-1/2'>
             <h1 className='text-7xl'> Our approach:</h1>
